62eb2b3b3ffcb832308044a70fee3421ac689b4e
mitrengamark <mitrengamark@edu.bme.hu>
2025-04-16 14:18:53 +0200

    Futnak a configok

--- run_configs.sh
+++ run_configs.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 CONFIG_DIR="Config/configs"
-CONFIGS=("ae3.ini" "ae4.ini" "ae5.ini" "ae6.ini")
+CONFIGS=("ae.ini" "ae3.ini" "ae4.ini" "ae5.ini" "ae6.ini")
 RUN_SCRIPT="python run.py"
 
 for CONFIG in "${CONFIGS[@]}"; do
     CONFIG_PATH="$CONFIG_DIR/$CONFIG"
-    CONFIG_NAME=$(basename "$CONFIG" .ini)
     LOGNAME="log_${CONFIG_NAME}_$(date +%Y%m%d_%H%M%S).log"
 
-    export CONFIG_NAME=$CONFIG_NAME
-
+    export CONFIG_PATH
     echo "Indítás: $CONFIG_PATH -> log: $LOGNAME"
-    $RUN_SCRIPT --config "$CONFIG_PATH" > "logs/$LOGNAME" 2>&1
+    $RUN_SCRIPT > "logs/$LOGNAME" 2>&1
 
     echo "Befejeződött: $CONFIG_PATH"
     echo ""
